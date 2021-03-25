de60da84af1510c8ea7b7e3e1f6dc906697e39ce
AlharranAbeer <AlharranAbeer@gmail.com>
2021-03-25 20:59:32 +0300

    Second

--- evaluate-news-nlp/src/server/index.js
+++ evaluate-news-nlp/src/server/index.js
@@ -18,10 +18,7 @@ app.use(express.static('dist'))
 
 app.get('/', function (req, res) {
     res.sendFile('dist/index.html')
-    // res.sendFile(path.resolve('src/client/views/index.html'))
 })
-
-// designates what port the app will listen to for incoming requests
 app.listen(8081, function () {
     console.log('Example app listening on port 8081!')
 })
